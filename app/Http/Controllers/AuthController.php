b708ed470ca81b0b58b336c5c1589ece06af60d6
andretraa <andretririzky123@gmail.com>
2023-07-20 10:03:20 +0700

    add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -104,7 +104,7 @@ class AuthController extends Controller
             $user->api_token = null;
             $user->save();
         }
-        Auth::guard('api')->logout();
+        Auth::guard('web')->logout();
     
         return response()->json([
             'status' => 'success',
